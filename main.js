6aa2737537af0368c9b28679fc4dae09920480d6
AlessioMaria5 <alessiomaria5@gmail.com>
2022-07-25 22:15:35 +0200

    ultimo bonus

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ myApp = new Vue({
     el: "#VueApp",
     data: {
 
+        myDataValue: "",
+
         toDoList: [
 
             {
@@ -25,32 +27,34 @@ myApp = new Vue({
     methods: {
 
         eliminazione(indice){
-            this.toDoList.splice(indice,1);
-            
+            this.toDoList.splice(indice,1);        
         },
 
         pushaElemento(valoreTesto){
-          this.toDoList.push({text:valoreTesto, done: false,});
+        this.toDoList.push({text:valoreTesto.trim(), done: true,});
+        this.myDataValue = " ";
+        
+        
+
         },
 
         doneEvent(indice){
-        //WAY 1 
-          
-        this.toDoList[indice].done = !this.toDoList[indice].done 
+        //WAY 1-1 
+
+        // this.toDoList[indice].done = !this.toDoList[indice].done 
          
-        // WAY 2
+        // WAY 1-2
 
-        // this.toDoList[indice].done = (this.toDoList[indice].done == true)? false : true;
-        console.log(this.toDoList[indice].done)
+        this.toDoList[indice].done = this.toDoList[indice].done == true? false : true;
         
-        //WAY 3
+        //WAY 1-3
 
-        // if(this.toDoList[indice].done == true){
-        //     this.toDoList[indice].done = false
-        // }   
-        // else if(this.toDoList[indice].done == false) {
-        //     this.toDoList[indice].done = true
-        // }
+        if(this.toDoList[indice].done == true){
+            this.toDoList[indice].done = false
+        }   
+        else if(this.toDoList[indice].done == false) {
+            this.toDoList[indice].done = true
+        }
         }
     },
 })
